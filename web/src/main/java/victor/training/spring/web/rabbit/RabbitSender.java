608dd8b46a07e3fc14c388405ff505e7d8aef517
Victor Rentea <victorrentea@gmail.com>
2023-05-18 06:55:36 +0300

    yolo

--- web/src/main/java/victor/training/spring/web/rabbit/RabbitSender.java
+++ web/src/main/java/victor/training/spring/web/rabbit/RabbitSender.java
@@ -1,6 +1,7 @@
 package victor.training.spring.web.rabbit;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,14 +9,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import static java.lang.Thread.sleep;
+
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class RabbitSender {
   private final RabbitTemplate rabbitTemplate;
+  @SneakyThrows
   public void sendMessage(String message) {
     log.info("Sending message: " + message);
+    sleep(10);
     rabbitTemplate.convertAndSend("rabbitqueue", message);
+    sleep(10);
     log.info("Sent");
   }
 
