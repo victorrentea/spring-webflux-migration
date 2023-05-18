8942b5c502a388b2df0f35858a7224e1a0ce69e3
Victor Rentea <victorrentea@gmail.com>
2023-05-18 07:06:21 +0300

    undo solution

--- flux/src/main/java/victor/training/spring/mongo/AuthorRepo.java
+++
@@ -1,6 +0,0 @@
-package victor.training.spring.mongo;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface AuthorRepo extends ReactiveMongoRepository<Author, String> {
-}
