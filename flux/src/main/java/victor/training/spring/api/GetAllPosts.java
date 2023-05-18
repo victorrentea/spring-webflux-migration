69c83f185f42aa224862a5b74528beebf962a8ce
Victor Rentea <victorrentea@gmail.com>
2023-05-18 19:11:29 +0300

    end

--- flux/src/main/java/victor/training/spring/api/GetAllPosts.java
+++ flux/src/main/java/victor/training/spring/api/GetAllPosts.java
@@ -2,18 +2,21 @@ package victor.training.spring.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import victor.training.spring.hibernate.Post;
 import victor.training.spring.hibernate.PostRepo;
+import victor.training.spring.jooq.Tables;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GetAllPosts { // #2
   private final PostRepo postRepo;
+  private final DSLContext jooq;
 
   public record GetPostsResponse(Long id, String title) {
     GetPostsResponse(Post post) {
@@ -22,8 +25,11 @@ public class GetAllPosts { // #2
   }
   @GetMapping("posts")
   public Flux<GetPostsResponse> getAllPosts() {
-    return Flux.fromStream(() -> postRepo.findAll().stream())
+    return
+        Flux.fromStream(() -> postRepo.findAll().stream())
         .subscribeOn(Schedulers.boundedElastic())
-        .map(GetPostsResponse::new);
+            .map(GetPostsResponse::new);
+//    return Flux.from(jooq.selectFrom(Tables.POST)).map(r->new GetPostsResponse(r.id))
+
   }
 }
