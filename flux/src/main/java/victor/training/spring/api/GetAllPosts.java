97c559aca88fffb7617362a869c63b5889511dcc
Victor Rentea <victorrentea@gmail.com>
2023-06-09 15:37:01 +0300

    added blockhound :) yeehaa

--- flux/src/main/java/victor/training/spring/api/GetAllPosts.java
+++ flux/src/main/java/victor/training/spring/api/GetAllPosts.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -14,6 +15,10 @@ import victor.training.spring.sql.PostRepo;
 @RestController
 @RequiredArgsConstructor
 public class GetAllPosts { // #2
+  static {
+    // add to VM options at startup: -XX:+AllowRedefinitionToAddDeleteMethods
+    BlockHound.install();
+  }
   private final PostRepo postRepo;
 
   public record GetPostsResponse(String id, String title) {
@@ -28,15 +33,11 @@ public class GetAllPosts { // #2
     // blockhound ar arunca exceptie cand chemi endpointul asta, ca blochezi
     // "legacy" non-reactive library call care nu tre sa ruleze AICI BLOCAND THREADUL
     log.info("Cine cheama functia efectiva"); // TODO de ce vad parallel- ? treabuia netti9qur2572857-
-    return Mono.fromCallable(() -> {
-          log.info("Unde sunt?");
-          return postRepo.findAll(); // "legacy" non-reactive library call
-        })
+    // "legacy" non-reactive library call
+    System.out.println(postRepo.findAll());
+    return Mono.fromCallable(postRepo::findAll)
         .subscribeOn(Schedulers.boundedElastic()) // subscribeul in sus unde ruleaza -> e pe alt thread acum
-        // si in continuare toate semnalele ulterioare
-        // (onNext, request,onerror, ompletion) raman pe bounded elastic
-        .log("semnale sub")
-        .flatMapMany(list -> Flux.fromIterable(list))
+        .flatMapMany(Flux::fromIterable)
         .map(GetPostsResponse::new);
   }
 }
