7793328b6539c370b0f9cad8f3e3a5799905d6c3
Victor Rentea <victorrentea@gmail.com>
2023-06-09 15:17:41 +0300

    cum folosesc ceva blocant (eg Hibernate) in webflux corect ca sa nu blochez threaduri: Mono.fromSupplier(()->blocant).subscribeOn(elastic)

--- flux/src/main/java/victor/training/spring/api/GetAllPosts.java
+++ flux/src/main/java/victor/training/spring/api/GetAllPosts.java
@@ -1,10 +1,12 @@
 package victor.training.spring.api;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import victor.training.spring.sql.Post;
 import victor.training.spring.sql.PostRepo;
 
@@ -19,8 +21,22 @@ public class GetAllPosts { // #2
       this(post.getId(), post.getTitle());
     }
   }
+
   @GetMapping("posts")
-  public List<GetPostsResponse> getAllPosts() {
-    return postRepo.findAll().stream().map(GetPostsResponse::new).toList();
+  public Flux<GetPostsResponse> getAllPosts() {
+    // findAll agata 1/2xCPU threaduri pe care cheama Netty metodele @RestController
+    // blockhound ar arunca exceptie cand chemi endpointul asta, ca blochezi
+    // "legacy" non-reactive library call care nu tre sa ruleze AICI BLOCAND THREADUL
+    log.info("Cine cheama functia efectiva"); // TODO de ce vad parallel- ? treabuia netti9qur2572857-
+    return Mono.fromCallable(() -> {
+          log.info("Unde sunt?");
+          return postRepo.findAll(); // "legacy" non-reactive library call
+        })
+        .subscribeOn(Schedulers.boundedElastic()) // subscribeul in sus unde ruleaza -> e pe alt thread acum
+        // si in continuare toate semnalele ulterioare
+        // (onNext, request,onerror, ompletion) raman pe bounded elastic
+        .log("semnale sub")
+        .flatMapMany(list -> Flux.fromIterable(list))
+        .map(GetPostsResponse::new);
   }
 }
