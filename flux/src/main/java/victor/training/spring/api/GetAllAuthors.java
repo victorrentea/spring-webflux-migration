e258c25dcb8772d9a6f357acedf4ec84b4360e9d
Victor Rentea <victorrentea@gmail.com>
2023-06-09 14:49:59 +0300

    get authors a mers

--- flux/src/main/java/victor/training/spring/api/GetAllAuthors.java
+++ flux/src/main/java/victor/training/spring/api/GetAllAuthors.java
@@ -1,5 +1,6 @@
 package victor.training.spring.api;
 
+import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,6 @@ import reactor.core.publisher.Mono;
 import victor.training.spring.mongo.Author;
 import victor.training.spring.mongo.AuthorRepo;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -21,22 +20,30 @@ public class GetAllAuthors { // #1
 
   @PostConstruct
   public void initialDataInMongo() {
-    authorRepo.save(new Author().setId(1000L).setName("John DOE").setBio("Long description")).block();
+    authorRepo
+        .save(new Author().setId(1000L).setName("John DOE").setBio("Long description"))
+        .block();
   }
 
   public record GetAuthorsResponse(Long id, String name, String email, String bio) { // JSON
     GetAuthorsResponse(Author author, String email) {
-      this(author.getId(), author.getName(),email, author.getBio());
+      this(author.getId(), author.getName(), email, author.getBio());
     }
   }
   @GetMapping("authors")
   public Flux<GetAuthorsResponse> getAllAuthors() {
-    return authorRepo.findAll()
-        .flatMap(author -> fetchEmail(author.getId()).map(email -> new GetAuthorsResponse(author, email)));
+    return authorRepo
+        .findAll()
+        .flatMap(
+            author ->
+                fetchEmail(author.getId())
+                    .map(email -> new GetAuthorsResponse(author, email)));
   }
+  
 
   private Mono<String> fetchEmail(Long authorId) {
-    return webClient.get()
+    return webClient
+        .get()
         .uri("http://localhost:9999/contact/" + authorId + "/email")
         .retrieve()
         .bodyToMono(String.class);
