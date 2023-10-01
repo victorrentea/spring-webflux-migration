60689942bb68b9c43cf15af39d6f75b82a799f58
Victor Rentea <victorrentea@gmail.com>
2023-10-02 00:47:39 +0300

    undo all

--- flux/src/main/java/victor/training/spring/sql/CommentRepo.java
+++
@@ -1,8 +0,0 @@
-package victor.training.spring.sql;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface CommentRepo extends ReactiveCrudRepository<Comment, Long> {
-  Flux<Comment> findByPostId(Long postId);
-}
--- flux/src/main/java/victor/training/spring/sql/PostRepo.java
+++
@@ -1,7 +0,0 @@
-package victor.training.spring.sql;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface PostRepo extends ReactiveCrudRepository<Post, Long> {
-
-}
