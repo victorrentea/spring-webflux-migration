e377e32ce662eae7cc2e68f25b816c12492fa069
Victor Rentea <victorrentea@gmail.com>
2023-10-02 13:10:54 +0300

    UNDO

--- flux/src/main/java/victor/training/spring/sql/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package victor.training.spring.sql;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-public interface CommentRepo extends R2dbcRepository<Comment, Long> {
-  Flux<Comment> findByPostId(Long postId);
-}
