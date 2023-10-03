ecc823c73b047ec40b778bbb6bc7d7cf256577e2
Victor Rentea <victorrentea@gmail.com>
2023-10-03 07:20:40 +0200

    undo solution

--- flux/src/main/java/victor/training/spring/sql/CommentRepo.java
+++
@@ -1,8 +0,0 @@
-package victor.training.spring.sql;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import reactor.core.publisher.Flux;
-
-public interface CommentRepo extends R2dbcRepository<Comment, Long> {
-  Flux<Comment> findByPostId(Long postId);
-}
